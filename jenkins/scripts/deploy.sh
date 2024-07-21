23f5ad548235115e02b024a33e367c818636f744
joachimsofjan <joachimsofjan@gmail.com>
2024-07-21 16:40:03 +0800

    CRLF

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env sh
-
 set -x
 docker run -d -p 80:80 --name my-apache-php-app -v C:\Users\Joachim\Documents\SIt\trim2.3\SSD\Lab\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
-
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
-
