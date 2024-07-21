672a61b07464829eea224c581bea3ece9c1e0fc7
joachimsofjan <joachimsofjan@gmail.com>
2024-07-21 16:47:34 +0800

    Http

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "192.168.18.42";
+	String url = "http://192.168.18.42:80";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
